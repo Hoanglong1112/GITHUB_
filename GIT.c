8f82df4d1f3f7fb95d3d93900bfc3f393597ac94
long <longhoangnguyen1112@gmail.com>
2025-08-26 16:00:40 +0700

    Xóa sạch toàn bộ file trong branch

--- GIT.c
+++
@@ -1,5 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("GIT");
-    return 0;
-}
--- TEST.c
+++
@@ -1,7 +0,0 @@
-#include<stdio.h>
-int main() {
-    printf("EMBEDEDD");
-    printf("DEV01");
-    printf("Stash");
-    return 0;
-}
